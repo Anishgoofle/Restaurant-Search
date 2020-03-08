847f6540c3e22cda317b760331610ce30cc41c08
Anish Ojha <anishojha82@gmail.com>
2020-03-08 15:15:10 +0530

    testing the ui

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Text, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
     return (
         <View>
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => console.log('term was submitted')}
+            />
             <Text>Search Screen</Text>
         </View>
     )
